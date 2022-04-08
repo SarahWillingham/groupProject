e9ff4e7c68d68c178bada4618ab9dd9c4fde1c18
SarahWillingham <102173982+SarahWillingham@users.noreply.github.com>
2022-04-08 08:10:06 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,54 +12,128 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		String usernameInput;
-		String passwordInput;
+		login();
+   
+	}
+	
+	
+	public static void login() {
+		String usernameInput = null;
+		String passwordInput = null;
+		int userId = 0;
+		int choice;
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Please enter username.");
 		usernameInput = sc.next();
 		System.out.println("Please enter password");
 		passwordInput = sc.next();
-		
-////		
-//		 String selectStatement = ("SELECT " + usernameInput + " from user;");
-//		 
-	     Connection conn = ConnectionManager.getConnection();
-//
-//	     ConnectionManager.setPreparedStatement(conn, selectStatement);
-//	     PreparedStatement ps = ConnectionManager.getPreparedStatement();
-//	     ps.execute();
-
+				 
+	    Connection conn = ConnectionManager.getConnection();
 		
 	     
-	     try (PreparedStatement pstmt =
-	    		 conn.prepareStatement("SELECT * from user where username = " + usernameInput + ";")){
-	    				
+	    try (PreparedStatement pstmt =
+	    		 conn.prepareStatement("SELECT * from user where user_username = '" + usernameInput + "';")){
 
-	    				boolean loginSuccess = false;
-	    				ResultSet userInfo = pstmt.executeQuery();
+	    	 boolean loginSuccess = false;
+	    	 ResultSet userInfo = pstmt.executeQuery();
+
+	    	 while(userInfo.next()) {
+	    		 userId = userInfo.getInt("user_id");
+	    		 if(userInfo.getString("user_password").equals(passwordInput)) {			
+	    			 loginSuccess = true;
+	    			 System.out.println("login successful!");
+	    			 System.out.println("user id = " + userId);
+	    		 }else {
+	    			 System.out.println("login failed!");
+	    		 }	    				
 	    				
-	    				
-	    				while(userInfo.next()) {
-	    					
-	    					if(userInfo.getString("user_password") == passwordInput) {
-	    						
-	    						loginSuccess = true;
-	    						System.out.println("login successful!");
-	    					
-	    					}else {
-	    					System.out.println("login failed!");
-	    				}	    				
-	    				
-	    			}
-	    		} catch(SQLException e) {
-	    				e.printStackTrace();
-	    			}
-	    			//no update occurred
-	    			//return false;
+	    	 }
+	    	 if (loginSuccess == true) {
+	    		 System.out.println("Do you want to: ");
+	    		 System.out.println("1. View/edit your watch list      2. View the database");
+	    		 choice = sc.nextInt();
+	    		 if(choice == 1) {
+	    			 dashboard(userId);
+	    		 }else if(choice == 2) {
+	    			 //function to send user to database
+	    		 }
+	    	 }
+			
+	     } catch(SQLException e) {
+	    	 e.printStackTrace();
+	     }  
+	}
+
+	
+	
+	public static void dashboard(int userId) throws SQLException {
 		
+
+	    Connection conn = ConnectionManager.getConnection();
+		PreparedStatement pstmt = conn.prepareStatement("SELECT t.user_id, s.show_id, s.show_title, t.status, t.tracker_id from tracker as t inner join shows as s on t.show_id = s.show_id where user_id = '" + userId + "';");
+		ResultSet rs = pstmt.executeQuery();
 		
+		while(rs.next()){
+			System.out.println("ID: " + rs.getString("t.tracker_id") + "  " + rs.getString("s.show_title") + ", STATUS: " + rs.getString("t.status"));
+		}
+				
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a number for your choice");
+		System.out.println("1: Update status of a show,  2: Search for a new show");
+		int choice = sc.nextInt();
+		if(choice == 1) {
+			updateStatus(userId);
+		}else if(choice == 2) {
+			// call search function here
+		
+		}
+		sc.close();
 		
 	}
+	
+	
+	public static void updateStatus(int userId) throws SQLException {
+		int newStatus;
+		String sqlStatement = null;
+		System.out.println("Select a tracker ID to update");	
+		
+		Connection conn = ConnectionManager.getConnection();
+		PreparedStatement pstmt = conn.prepareStatement("SELECT t.user_id, s.show_id, s.show_title, t.status, t.tracker_id from tracker as t inner join shows as s on t.show_id = s.show_id where user_id = '" + userId + "';");
+
+		ResultSet rs = pstmt.executeQuery();
+
+
+		
+		Scanner sc = new Scanner(System.in);
+		int updateId = sc.nextInt();
+		System.out.println("updateId = " + updateId);
+		System.out.println("Select new status: 1 for \"in progress\" or 2 for \"finished\"");
+		newStatus = sc.nextInt();
+		
+		if(newStatus == 1) {
+			sqlStatement = "update tracker set status = 'in progress' where tracker_id = " + updateId + ";";
+		} else if(newStatus == 2) {
+			sqlStatement = "update tracker set status = 'finished' where tracker_id = " + updateId + ";";
+		}
+		
+		
+		PreparedStatement ps = conn.prepareStatement(sqlStatement);
+		ps.execute();
+		
+		dashboard(userId);
+	}
+	
+	
+	public void addToWatchlist(int trackerId) {
+		
+	}
+	
+	
+	public void removeFromWatchList(int trackerId) {
+		
+	}
+	
 
 }
