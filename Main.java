28bcc6e370b32da841be237f333efb9b5c82904f
SarahWillingham <102173982+SarahWillingham@users.noreply.github.com>
2022-04-08 13:31:22 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 package Controller;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Scanner;
@@ -52,7 +49,7 @@ public class Main {
 	    	 }
 	    	 if (loginSuccess == true) {
 	    		 System.out.println("Do you want to: ");
-	    		 System.out.println("1. View/edit your watch list      2. View the database		3. Logout	4. Change username/password");
+	    		 System.out.println("1. View/edit your watch list \n2. View the database \n3. Logout \n4. Change password");
 	    		 choice = sc.nextInt();
 	    		 if(choice == 1) {
 	    			 dashboard(userId);
@@ -86,7 +83,7 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Enter a number for your choice");
-		System.out.println("1: Update status of a show,  2: Search for a new show, 3: Logout, 4: change username/password");
+		System.out.println("1: Update status of a show  2: Search for a new show 3: Logout 4: change password");
 		int choice = sc.nextInt();
 		if(choice == 1) {
 			updateStatus(userId);
@@ -94,11 +91,46 @@ public class Main {
 			displayShowsDB(userId);
 		}else if(choice == 3) { 
 			login();
+		}else if(choice == 4) {
+			updateUserPassword(userId);
 		}
 		sc.close();
 		
 	}
 	
+	
+	public static void updateStatus(int userId) throws SQLException {
+		int newStatus;
+		String sqlStatement = null;
+		System.out.println("Select a tracker ID to update");	
+		
+		Connection conn = ConnectionManager.getConnection();
+		PreparedStatement pstmt = conn.prepareStatement("SELECT t.user_id, s.show_id, s.show_title, t.status, t.tracker_id from tracker as t inner join shows as s on t.show_id = s.show_id where user_id = '" + userId + "';");
+
+		pstmt.executeQuery();
+		
+		Scanner sc = new Scanner(System.in);
+		int updateId = sc.nextInt();
+		System.out.println("updateId = " + updateId);
+		System.out.println("Select new status: 1 for 'in progress' or 2 for 'finished' or 3 to remove from your watch list or 4 to go back");
+		newStatus = sc.nextInt();
+		
+		if(newStatus == 1) {
+			sqlStatement = "update tracker set status = 'in progress' where tracker_id = " + updateId + ";";
+		} else if(newStatus == 2) {
+			sqlStatement = "update tracker set status = 'finished' where tracker_id = " + updateId + ";";
+		} else if(newStatus == 3) {
+			removeFromWatchList(updateId, userId);
+		} else if(newStatus == 4) {
+			dashboard(userId);
+		}
+		
+		
+		PreparedStatement ps = conn.prepareStatement(sqlStatement);
+		ps.execute();
+		
+		dashboard(userId);
+	}
 	
 	
 	public static void displayShowsDB(int userId) throws SQLException {
@@ -112,56 +144,54 @@ public class Main {
     	 }
 		
 		System.out.println("Select an option:");
-		System.out.println("1: Search for a show    2: Add show to watch List    3: Go to watch list");
+		System.out.println("1: Add show to watch List    2: Go to watch list");
 		
 		Scanner sc = new Scanner(System.in);
 		int choice = sc.nextInt();
 		
 		if(choice == 1) {
-			//allow user to search
+			System.out.println("Enter the ID number of the show you want to add");
+			int showId = sc.nextInt();
+			addToWatchList(showId, userId);
 		}else if(choice == 2) {
-			//add show to watch list
-		}else if(choice == 3) {
 			dashboard(userId);
 		}
 		
 	}
 	
+
+	
+    public static void addToWatchList(int showId, int userId) throws SQLException{
+    	
+    	Connection conn = ConnectionManager.getConnection();
+
+    	String sql = "INSERT INTO tracker(user_id, show_id, status) values (" + userId + ", " + showId + ", 'not started');";
+    	PreparedStatement pstmt = conn.prepareStatement(sql);
+    	int row = pstmt.executeUpdate();
+    		
+   		if(row > 0) {
+   			System.out.println("A new show has been added successfully!");
+   		}else {
+   			System.out.println("Failed to add show.");
+   		}
+   		
+  		conn.close();
+  		
+   		displayShowsDB(userId);
+   		
+    }
 	
 	
-	public static void updateStatus(int userId) throws SQLException {
-		int newStatus;
-		String sqlStatement = null;
-		System.out.println("Select a tracker ID to update");	
+	public static void removeFromWatchList(int trackerId, int userId) throws SQLException {
 		
-		Connection conn = ConnectionManager.getConnection();
-		PreparedStatement pstmt = conn.prepareStatement("SELECT t.user_id, s.show_id, s.show_title, t.status, t.tracker_id from tracker as t inner join shows as s on t.show_id = s.show_id where user_id = '" + userId + "';");
-
-		pstmt.executeQuery();
-
-
-		
-		Scanner sc = new Scanner(System.in);
-		int updateId = sc.nextInt();
-		System.out.println("updateId = " + updateId);
-		System.out.println("Select new status: 1 for 'in progress' or 2 for 'finished' or 3 to remove from your watch list or 4 to go back");
-		newStatus = sc.nextInt();
-		
-		if(newStatus == 1) {
-			sqlStatement = "update tracker set status = 'in progress' where tracker_id = " + updateId + ";";
-		} else if(newStatus == 2) {
-			sqlStatement = "update tracker set status = 'finished' where tracker_id = " + updateId + ";";
-		} else if(newStatus == 3) {
-			sqlStatement = "delete from tracker where tracker_id = " + updateId + ";";
-		} else if(newStatus == 4) {
-			dashboard(userId);
-		}
-		
-		
-		PreparedStatement ps = conn.prepareStatement(sqlStatement);
-		ps.execute();
-		
-		dashboard(userId);
+    	Connection conn = ConnectionManager.getConnection();
+    	PreparedStatement pstmt = conn.prepareStatement("DELETE from tracker where tracker_id = " + trackerId + ";");	
+   		int showRemoved = pstmt.executeUpdate();
+   		if(showRemoved > 0) {
+   			System.out.println("Show has been removed!");    			
+   		}
+   		
+   		dashboard(userId);
 	}
 	
 	
